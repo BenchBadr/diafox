68aa5bf717271286856fce02922393c02a27301b
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2025-08-11 15:40:13 +0200

    beta suggestoin selection

--- sidebar/messaging.js
+++ sidebar/messaging.js
@@ -33,35 +33,47 @@ const highlightSkills = async (root) => {
  * 
  * @param {*} selection 
  */
-const getSuggestions = (selection) => {
+const getSuggestions = (selection, activeIdx) => {
     suggestions.innerHTML = '';
-    
+
     const range = selection.getRangeAt(0);
     const current = range.startContainer.textContent.slice(0, range.startOffset).split(' ');
     const command = current[current.length - 1];
 
-    console.log('commands',command)
+    let suggesting = false;
     
     if (command.startsWith('/')) {
-        suggestSkills(command.slice(1));
+        suggesting = suggestSkills(command.slice(1), activeIdx);
     }
+    
+    return suggesting;
 }
 
 
-const suggestSkills = async (query) => {
+const suggestSkills = async (query, activeIdx) => {
     const skills = await getCookie('skills');
-    const skillsNames = Object.keys(skills)
+    const skillsNames = Object.keys(skills);
 
-    skillsNames.forEach(skill => {
+    let suggesting = 0;
+
+    skillsNames.forEach((skill, index) => {
         if (skill === 'undefined') return;
-        console.log('QS',query,skill)
+
         if (skill.startsWith(query)) {
             const span = document.createElement('span');
-            span.classList.add('res-skill')
+            span.classList.add('res-skill');
+            if (index === activeIdx) {
+                span.classList.add('active');
+            }
+
             span.textContent = skill;
             suggestions.appendChild(span);
+
+            suggesting++;
         }
     });
+
+    return suggesting;
 }
 
 
@@ -85,13 +97,30 @@ textarea.oninput = (event) => {
 };
 
 
-const logCaret = () => {
-    console.log('caret moved')
+let suggesting = 0;
+let activeIdx = 0;
 
+const logCaret = async () => {
     // renew / clear suggestions
     const selection = window.getSelection();
     if (selection.isCollapsed) {
-        getSuggestions(selection);
+        suggesting = await getSuggestions(selection, activeIdx);
+    }
+
+    if (suggesting) {
+        const handleArrow = (e) => {
+            if (e.key === 'ArrowUp') {
+                e.preventDefault();
+                activeIdx = (activeIdx + 1) % suggesting;
+            }
+
+            if (e.key === 'ArrowDown') {
+                e.preventDefault();
+                activeIdx = (activeIdx + 1) % suggesting;
+            }
+            console.log(activeIdx, suggesting)
+        };
+        textarea.addEventListener('keydown', handleArrow, { once: true });
     }
 }
 
