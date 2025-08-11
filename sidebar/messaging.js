2b069c7814a84347fc1627335776f1fa2a8b6571
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2025-08-11 15:28:44 +0200

    suggestions

--- sidebar/messaging.js
+++ sidebar/messaging.js
@@ -1,6 +1,8 @@
 import { highlight } from './highlight.js';
 
 
+const suggestions = document.querySelector('.suggestions');
+
 function sanitizeHtml(div) {
     const allowedTags = ['BR', 'MARK'];
     Array.from(div.childNodes).forEach(node => {
@@ -24,11 +26,43 @@ const highlightSkills = async (root) => {
     }
 }
 
+
+
+/**
+ * Retrieves suggestions whether it's a tab mention or skill
+ * 
+ * @param {*} selection 
+ */
 const getSuggestions = (selection) => {
+    suggestions.innerHTML = '';
+    
     const range = selection.getRangeAt(0);
-    console.log(range.startContainer);
+    const current = range.startContainer.textContent.slice(0, range.startOffset).split(' ');
+    const command = current[current.length - 1];
+
+    console.log('commands',command)
+    
+    if (command.startsWith('/')) {
+        suggestSkills(command.slice(1));
+    }
 }
 
+
+const suggestSkills = async (query) => {
+    const skills = await getCookie('skills');
+    const skillsNames = Object.keys(skills)
+
+    skillsNames.forEach(skill => {
+        if (skill === 'undefined') return;
+        console.log('QS',query,skill)
+        if (skill.startsWith(query)) {
+            const span = document.createElement('span');
+            span.classList.add('res-skill')
+            span.textContent = skill;
+            suggestions.appendChild(span);
+        }
+    });
+}
 
 
 textarea.oninput = (event) => {
@@ -40,10 +74,6 @@ textarea.oninput = (event) => {
 
     // highlight(div, 'test')
 
-    const selection = window.getSelection();
-    if (selection.isCollapsed) {
-        getSuggestions(selection);
-    }
 
     // Update send button state based
     const promptValue = div.textContent || '';
@@ -53,3 +83,18 @@ textarea.oninput = (event) => {
         sendBtn.classList.add('empty');
     }
 };
+
+
+const logCaret = () => {
+    console.log('caret moved')
+
+    // renew / clear suggestions
+    const selection = window.getSelection();
+    if (selection.isCollapsed) {
+        getSuggestions(selection);
+    }
+}
+
+
+textarea.addEventListener('keyup', logCaret);
+textarea.addEventListener('click', logCaret);
